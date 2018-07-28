a49c8f5db379379bd940f7a5a5c9ed925239d807
Goran Mekić <meka@tilda.center>
2018-07-28 04:15:17 +0200

    Update package list

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-pkg install -y ansible python
-ansible-playbook -i provision/inventory/localhost provision/site.yml -c local
+pkg install -y py36-ansible python36
+ansible-playbook-3.6 -i provision/inventory/localhost provision/site.yml -c local
